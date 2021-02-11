4e10094adbf7db795052be8c549eb99c3337213a
西脇佳宏 <nishiwakiyoshihiro@nishiwakiyoshihironoMacBook-Air.local>
2021-02-11 14:38:33 +0900

    providerでコンテキストを変更する

--- src/App.js
+++ src/App.js
@@ -2,65 +2,50 @@ import React, { Component } from 'react';
 import Rect from './Rect';
 import './App.css';
 
+let data = {title: 'Title', message: 'this is sample message'};
+
+const SampleContext = React.createContext(data);
+
 class App extends Component {
-  input = '';
-
-  msgStyle = {
-    fontSize: "20pt",
-    color: "#900",
-    margin: "20px 0px",
-    padding: "5px",
-  }
-
-  inputStyle = {
-    fontSize: "12px",
-    padding: "5px",
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: 'type your name:'
-    };
-    this.doCheck = this.doCheck.bind(this);
-  }
-
-  doCheck(event) {
-    alert(event.target.value + "は長すぎます。(最大10文字)");
-  }
+  newdata = {title: '新しいタイトル', message: 'これは新しいメッセージです'};
 
   render() {
-    return (
+    return(
       <div>
-        <h1>React</h1>
-        <h2>{this.state.message}</h2>
-        <Message maxLength="10" onCheck={this.doCheck} />
+        <h1>Context</h1>
+        <Title />
+        <Message />
+        <SampleContext.Provider value={this.newdata}>
+          <Title/>
+          <Message/>
+        </SampleContext.Provider>
+          <Title/>
+          <Message/>
       </div>
-    );
+    )
+  }
+}
+
+class Title extends Component {
+  static contextType = SampleContext;
+
+  render() {
+    return(
+      <div>
+        <h2>{this.context.title}</h2>
+      </div>
+    )
   }
 }
 
 class Message extends Component {
-  inputStyle = {
-    fontSize: "12px",
-    padding: "5px",
-  }
-
-  constructor(props) {
-    super(props);
-    this.doChange = this.doChange.bind(this);
-  }
-
-  doChange(e) {
-    if (e.target.value.length > this.props.maxLength) {
-      this.props.onCheck(e);
-      e.target.value = e.target.value.substr(0, this.props.maxLength);
-    }
-  }
+  static contextType = SampleContext;
 
   render() {
-    return (
-      <input type = "text" style = {this.inputStyle} onChange = {this.doChange} />
+    return(
+      <div>
+        <p>{this.context.message}</p>
+      </div>
     );
   }
 }
